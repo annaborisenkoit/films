3173809b906e3b03356d106fe61ed574ebff97f0
Anna Borisenko <anik4366@mail.ru>
2023-06-23 10:46:33 +0700

    подготовка к LS

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const filmAddInputNode = document.querySelector('.js-add__input');
 const newFilmBtnNode = document.querySelector('.js-add__btn');
 const filmsNode = document.querySelector('.js-films');
 
+let films = [];
+
 //добавление фильма
 addFormNode.addEventListener('submit', addFilm);
 
@@ -24,6 +26,19 @@ function addFilm(event) {
   //обрезаем пробелы - trim
   const filmTitle = filmAddInputNode.value.trim();
 
+  //формируем объект для LS
+  const newFilm = {
+    id: Date.now(),
+    text: filmTitle,
+    done: false,
+  };
+
+  //добавляем новый фильм (объект) в массив с фильмами (для LS)
+  films.push(newFilm);
+
+  //формируем css класс - тернарный оператор
+  const cssClass = newFilm.done ? 'checked' : '';
+
   //проверяем, не пустой ли инпут
   if (!filmTitle) {
     alert('Необходимо ввести название фильма');
@@ -31,11 +46,10 @@ function addFilm(event) {
   }
 
   //формируем разметку для нового фильма
-
-  const filmHTML = `<li class="js-film film">
+  const filmHTML = `<li id="${newFilm.id}" class="js-film film ${cssClass}">
                    
                       <input type="checkbox"class="js-film__checkbox film__checkbox" data-action="done">
-                      <p class="film__text">${filmTitle}</p>
+                      <p class="film__text">${newFilm.text}</p>
                       <button class="close-btn" data-action="delete"></button>
                    
                 </li>`;
@@ -51,11 +65,18 @@ function addFilm(event) {
 
 function deleteFilm(event) {
   //проверяем, что клик был по кнопке удаления
-  console.log(event.target);
 
   if (event.target.dataset.action === 'delete') {
     const parentNode = event.target.closest('.film');
 
+    //определяем id фильма в виде числа
+    const id = Number(parentNode.id);
+
+    //отфильтруем массив, чтобы новый фильм в него не попал, который - false
+    //каждый элемент массива обозначим как film
+    films = films.filter((film) => film.id !== id);
+
+    //удаляем фильм из разметки
     parentNode.remove();
   }
 }
@@ -63,6 +84,19 @@ function deleteFilm(event) {
 function viewedFilm(event) {
   if (event.target.dataset.action === 'done') {
     const parentNode = event.target.closest('.film');
+
+    //определяем id фильма в виде числа
+    const id = Number(parentNode.id);
+
+    //метод find возвращает сам найденный элемент - при true
+    const film = films.find(function (film) {
+      if (film.id === id) {
+        return true;
+      }
+    });
+
+    film.done = !film.done;
+
     parentNode.classList.toggle('checked');
   }
 }
